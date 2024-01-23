ca53e4d7c7e82b5cf0c580992b81e0f0ea768e06
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-01-23 03:44:04 -0800

    files created

--- sort.h
+++ sort.h
@@ -2,6 +2,8 @@
 #define SORT_H
 
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,7 +21,6 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
